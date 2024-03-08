6cc30af0399993036635a5ce8d9dc6cee8b671ed
Michelle Lim <maydayvesper40@gmail.com>
2024-03-08 22:31:18 +0800

    added DoctorList, partially working

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,8 +58,6 @@ dependencies {
     implementation(platform("androidx.compose:compose-bom:2023.08.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
-    implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3")
     implementation ("androidx.navigation:navigation-compose:2.7.7")
     implementation("com.google.firebase:firebase-auth:22.3.1")
     testImplementation("junit:junit:4.13.2")
@@ -69,4 +67,6 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+    implementation("androidx.compose.material3:material3:1.2.1")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.6.3")
 }
--- app/src/main/java/com/example/myapplication/PublicBooking.kt
+++ app/src/main/java/com/example/myapplication/PublicBooking.kt
@@ -1,11 +1,24 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.example.myapplication
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.BasicText
 import androidx.compose.material3.Button
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -13,6 +26,9 @@ import androidx.compose.ui.unit.dp
 
 @Composable
 fun PublicBooking() {
+
+    var isDoctorListExpanded by remember { mutableStateOf(false)}
+
     Surface(color = Color.White) {
         Column(
             modifier = Modifier
@@ -21,9 +37,12 @@ fun PublicBooking() {
         ) {
             GreetingSection()
             ScheduleAppointmentSection()
-            Button(onClick = { /* Do something */ }, modifier = Modifier.fillMaxWidth()) {
+            Button(onClick = { isDoctorListExpanded = true }, modifier = Modifier.fillMaxWidth()) {
                 BasicText(text = "Doctor Name")
             }
+            if (isDoctorListExpanded) {
+                    DoctorList()
+                }
             Button(onClick = { /* Do something */ }, modifier = Modifier.fillMaxWidth()) {
                 BasicText(text = "Date")
             }
@@ -61,6 +80,74 @@ fun ScheduleAppointmentSection() {
             .padding(bottom = 32.dp)
     )
 }
+
+//function for Doctor Name button
+@Composable
+fun DoctorList() {
+    var isExpanded by remember {
+        mutableStateOf(false)
+    }
+    var doctorName by remember {
+        mutableStateOf("")
+    }
+
+    Box{
+        ExposedDropdownMenuBox(
+            expanded = isExpanded,
+            onExpandedChange ={ isExpanded = it}
+        ) {
+            TextField(
+                value = doctorName,
+                onValueChange = {},
+                readOnly = true,
+                // when expanded, it doesn't show the doctor names but when clicked on the
+                //empty space, the Doctor's name selected
+                // ie, expanded menu displays white drop down box, clicked on white space, "Dr.Anne" shows up
+                trailingIcon = {
+                    ExposedDropdownMenuDefaults.TrailingIcon(expanded = isExpanded)
+                },
+                colors = ExposedDropdownMenuDefaults.textFieldColors(),
+                modifier = Modifier.menuAnchor()
+            )
+
+            ExposedDropdownMenu(
+                expanded = isExpanded,
+                onDismissRequest = { isExpanded = false }
+            ) {
+                DropdownMenuItem(
+                    text = { "Dr.Anne" },
+                    onClick = {
+                        doctorName = "Dr.Anne"
+                        isExpanded = false
+                    }
+                )
+                DropdownMenuItem(
+                    text = { "Dr.Sue" },
+                    onClick = {
+                        doctorName = "Dr.Sue"
+                        isExpanded = false
+                    }
+                )
+                DropdownMenuItem(
+                    text = { "Dr.David" },
+                    onClick = {
+                        doctorName = "Dr.David"
+                        isExpanded = false
+                    }
+                )
+                DropdownMenuItem(
+                    text = { "Dr.John" },
+                    onClick = {
+                        doctorName = "Dr.John"
+                        isExpanded = false
+                    }
+                )
+            }
+
+        }
+    }
+}
+
 
 @Preview
 @Composable
